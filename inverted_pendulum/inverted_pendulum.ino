0440ce84d2e56e3ae54c01bfbe035e153f3d4569
sota <mannnen271828@gmail.com>
2026-02-28 23:40:50 +0900

    角度フィードバックのみで立った

--- inverted_pendulum/inverted_pendulum.ino
+++ inverted_pendulum/inverted_pendulum.ino
@@ -17,6 +17,7 @@ MCP_CAN CAN(CAN_CS_PIN);
 
 // --- CyberGear用の設定 ---
 uint8_t MOTOR_ID_1 = 0x01; // 1つ目のモーターID
+uint8_t MOTOR_ID_2 = 0x02; // 2つ目のモーターID
 uint8_t MASTER_ID = 0x00;  // ESP32側のID (0x00などでOK)
 
 // CyberGearクラスのインスタンス
@@ -24,7 +25,7 @@ CyberGear *cybergear;
 
 // カスケードPIDコントローラーのインスタンス
 PID velocityPI(0.1f, 0.01f, 0.0f, 0.0f, -0.785f, 0.785f); // 入力速度のPI制御（目標速度 - 実測速度）用，出力目標角度制限は±45度（0.785 rad/s）程度を想定
-PID posturePD(20.0f, 0.0f, 0.25f, 0.0f, -30.0f, 30.0f);   // 姿勢制御用のPDコントローラー（目標角度 - 実測角度）用，直接IMUの角度微分をD項に使用するため不完全微分は不要、出力は速度指令なので±30 rad/s程度を想定
+PID posturePD(50.0f, 0.0f, 0.025f, 0.0f, -30.0f, 30.0f);   // 姿勢制御用のPDコントローラー（目標角度 - 実測角度）用，直接IMUの角度微分をD項に使用するため不完全微分は不要、出力は速度指令なので±30 rad/s程度を想定
 
 // ICM42688センサーのインスタンス
 ICM42688 IMU(SPI, IMU_CS_PIN);
@@ -58,17 +59,26 @@ void setup()
 
   // モーターを登録（モーター番号0にCAN ID 0x01のモーターを登録）
   cybergear->addMotor(0, MOTOR_ID_1);
+  delay(100);
+  cybergear->addMotor(1, MOTOR_ID_2);
+  delay(100);
 
   // モーターを初期化（速度モード）
   cybergear->init_motor(0, MODE_SPEED);
+  delay(100);
+  cybergear->init_motor(1, MODE_SPEED);
   delay(100);
 
   // モーターのゼロ位置を設定
   cybergear->set_mechpos_zero(0);
   delay(100);
+  cybergear->set_mechpos_zero(1);
+  delay(100);
 
   // モーターの速度制限を設定（rad/s）
   cybergear->set_limit_speed(0, 30.0);
+  delay(100);
+  cybergear->set_limit_speed(1, 30.0);
   delay(100);
 
   // Serial.println("CyberGear Motors Initialized!"); // enable_motorはloop内で開始後N秒で行う
@@ -118,6 +128,7 @@ void loop()
   if (!motorEnabled && millis() >= INITIAL_WAIT_SEC * 1000.0)
   {
     cybergear->enable_motor(0);
+    cybergear->enable_motor(1);
     motorEnabled = true;
     Serial.println("Motor Enabled!");
   }
@@ -130,54 +141,56 @@ void loop()
   {
     lastSpeedUpdateTime = currentTime;
 
-    target_angle = velocityPI.update_pi(0.0f, cybergear->getSpeed(0), SPEED_UPDATE_INTERVAL_US / 1000000.0f);                                         // 目標速度は0、実測速度はモーターから取得
-    target_speed = posturePD.update_pd_measurement_deriv(target_angle, madwickFilter.getAngle(), -IMU.gyrZ(), SPEED_UPDATE_INTERVAL_US / 1000000.0f); // 目標角度 - 実測角度を入力、ジャイロの角速度をD項に使用
+    // target_angle = velocityPI.update_pi(0.0f, cybergear->getSpeed(0), SPEED_UPDATE_INTERVAL_US / 1000000.0f);                                         // 目標速度は0、実測速度はモーターから取得
+    target_angle = 0;
+    target_speed = posturePD.update_pd_measurement_deriv(target_angle, madwickFilter.getAngle(), -IMU.gyrY(), SPEED_UPDATE_INTERVAL_US / 1000000.0f); // 目標角度 - 実測角度を入力、ジャイロの角速度をD項に使用
 
     // 目標速度を設定 (最初のN秒間は指令を出さない)
     if (motorEnabled)
     {
-      cybergear->set_speed(0, target_speed);
+      cybergear->set_speed(0, -target_speed);
+      cybergear->set_speed(1, target_speed);
     }
-  }
 
-  // 指定間隔ごとにステータスを要求して表示
-  if (currentTime - lastStatusTime >= STATUS_REQUEST_INTERVAL_US)
-  {
-    lastStatusTime = currentTime;
+    // 指定間隔ごとにステータスを要求して表示
+    if (currentTime - lastStatusTime >= STATUS_REQUEST_INTERVAL_US)
+    {
+      lastStatusTime = currentTime;
 
-    // モーター1のステータスを要求
-    cybergear->request_status(0);
-    delayMicroseconds(250);
-    cybergear->process_can_message();
-  }
+      // モーター1のステータスを要求
+      cybergear->request_status(0);
+      delayMicroseconds(250);
+      cybergear->process_can_message();
+    }
 
-  // 指定間隔ごとにIMUデータを読み取って表示
-  if (currentTime - lastIMUTime >= IMU_UPDATE_INTERVAL_US)
-  {
-    lastIMUTime = currentTime;
+    // 指定間隔ごとにIMUデータを読み取って表示
+    if (currentTime - lastIMUTime >= IMU_UPDATE_INTERVAL_US)
+    {
+      lastIMUTime = currentTime;
 
-    // IMUデータを読み取る
-    IMU.getAGT();
+      // IMUデータを読み取る
+      IMU.getAGT();
 
-    // Madgwickフィルタを更新
-    madwickFilter.update(IMU.accZ(), IMU.accX(), -IMU.gyrY(), currentTime);
-  }
+      // Madgwickフィルタを更新
+      madwickFilter.update(IMU.accZ(), IMU.accX(), -IMU.gyrY(), currentTime);
+    }
 
-  if (currentTime - lastPrintTime >= PRINT_INTERVAL_US)
-  {
-    lastPrintTime = currentTime;
-    // 重力加速度から角度を計算する
+    if (currentTime - lastPrintTime >= PRINT_INTERVAL_US)
+    {
+      lastPrintTime = currentTime;
+      // 重力加速度から角度を計算する
 
-    // angle, angular_velocity, motor_speed, target_angle, target_speed
-    Serial.print(madwickFilter.getAngle(), 3);
-    Serial.print(", ");
-    Serial.print(-IMU.gyrY(), 3);
-    Serial.print(", ");
-    Serial.print(cybergear->getSpeed(0), 3);
-    Serial.print(", ");
-    Serial.print(target_angle, 3);
-    Serial.print(", ");
-    Serial.print(target_speed, 3);
-    Serial.println();
+      // angle, angular_velocity, motor_speed, target_angle, target_speed
+      Serial.print(madwickFilter.getAngle(), 3);
+      Serial.print(", ");
+      Serial.print(-IMU.gyrY(), 3);
+      Serial.print(", ");
+      Serial.print(cybergear->getSpeed(0), 3);
+      Serial.print(", ");
+      Serial.print(target_angle, 3);
+      Serial.print(", ");
+      Serial.print(target_speed, 3);
+      Serial.println();
+    }
   }
 }
